84e340b1c97d399be8d5a524373ee1adb49e130f
Peter Hunnisett <peter@softwarebalm.com>
2019-10-23 22:07:35 -0600

    Add dynamic path segments and query strings to pretty router.

--- src/pretty.ts
+++ src/pretty.ts
@@ -4,6 +4,62 @@ export interface IPrettyRoute extends Route {
 	linkClick: (event: MouseEvent) => void;
 }
 
+export interface IPrettyRouteDynamicSegments {
+	[route: string]: string;
+}
+
+export interface IPrettyRouteQueries {
+	[field: string]: string | undefined;
+}
+
+const dynamicRoutes: IPrettyRouteDynamicSegments = {};
+
+function decomposeDynamicRoute(route: string): {staticSegment: string, dynamicSegment: string } {
+	let pos = route.search(/[:\?]/);
+	if(pos === -1) {
+		pos = route.length;
+	} else if(route[pos] === ":") {
+		// Need to capture the "/"" before the ":". It must exist to be a well formed path element.
+		pos = pos - 1;
+	}
+
+	return {
+		dynamicSegment: route.substring(pos),
+		staticSegment: route.substring(0, pos),
+	};
+}
+
+export function addDynamicRoute(route: string): string {
+	const {staticSegment, dynamicSegment} = decomposeDynamicRoute(route);
+
+	// Just in case someone passes in a static route - ignore it.
+	if(dynamicSegment) {
+		dynamicRoutes[staticSegment] = dynamicSegment;
+	}
+
+	return staticSegment;
+}
+
+export function removeDynamicRoute(route: string): string | undefined {
+	const {staticSegment, dynamicSegment} = decomposeDynamicRoute(route);
+
+	const old = dynamicRoutes[staticSegment];
+
+	if(old) {
+		delete dynamicRoutes[staticSegment];
+		return staticSegment;
+	}
+
+	return undefined;
+}
+
+let prettyLinkQuerySeparator = "&";
+export function setPrettyLinkQuerySeparator(newSeparator: string): string {
+	const old = prettyLinkQuerySeparator;
+	prettyLinkQuerySeparator = newSeparator;
+	return old;
+}
+
 // Handle automatically setting the pretty link onclick handler
 // via the $prettylink directive
 const prettyClickDirective: string = "$prettylink";
@@ -16,12 +72,53 @@ app.on("$", ({key, props}) => {
 	});
 
 // A router function that handles "pretty links" (i.e. non hash based urls)
-// and html5 history.
+// dynamic segements, query strings, and html5 history.
 function prettyLinkRouter(url: string, popstate: boolean = false): void {
-	const [_, name, ...rest] = url.split("/");
+	// Adjust url in case there is a dynamic portion such as a query or
+	// dynamic segment.
 
-	app.run("/" + name, ...rest) || app.run(ROUTER_404_EVENT, "/" + name, ...rest);
-	app.run(ROUTER_EVENT, "/" + name, ...rest);
+	// Any queries?
+	let queries: {[name: string]: string} | undefined;
+
+	const queryPos = url.search(/\?/);
+	if(queryPos >= 0) {
+		const queryStr = url.substring(queryPos + 1);
+		const queryStrings = queryStr.split(prettyLinkQuerySeparator);
+		queries = {};
+		for(const query of queryStrings) {
+			const [field, value] = query.split("=");
+			queries[field] = value;
+		}
+
+		url = url.substring(0, queryPos);
+	}
+
+	// Any dynamic segements?
+	let dynamic: {[name: string]: string} | undefined;
+	const matchingDynamicRoute = Object.keys(dynamicRoutes).find((route) => {
+		return url.startsWith(route + "/");
+	});
+	if(matchingDynamicRoute) {
+		const urlDynamicParams = url.substring(matchingDynamicRoute.length).split("/");
+		const dynamicParams = dynamicRoutes[matchingDynamicRoute].split("/");
+
+		console.assert(dynamicParams.length === urlDynamicParams.length, `expected dynamic and actual dynamic length don't match ${dynamicParams.length} vs. ${urlDynamicParams.length}`);
+
+		dynamic = {};
+
+		// First param will always be a "", so ignore it.
+		for(let i = 1; i < dynamicParams.length; ++i) {
+			// Is this parameter in the dynamic section actually dynamic?
+			if(dynamicParams[i][0] === ":") {
+				dynamic[dynamicParams[i].substring(1)] = urlDynamicParams[i];
+			}
+		}
+
+		url = matchingDynamicRoute;
+	}
+
+	app.run(url, dynamic, queries) || app.run(ROUTER_404_EVENT, url, dynamic, queries);
+	app.run(ROUTER_EVENT, url, dynamic, queries);
 
 	if(!popstate) history.pushState({}, "", url);
 }
